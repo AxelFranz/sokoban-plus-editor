435be066eed08c2596d1f0f3bb8ce92cbcbbe804
FRANZ AXEL <axel.franz@etu.unistra.fr>
2023-01-16 11:47:24 +0100

    Vrai fix :)

--- SDL2/build/SDL_atomic.d
+++
@@ -1,8 +0,0 @@
-build/SDL_atomic.lo: \
- /home/axel/techdev-sokoban-franz/SDL2/src/atomic/SDL_atomic.c \
- /home/axel/techdev-sokoban-franz/SDL2/src/atomic/../SDL_internal.h \
- /home/axel/techdev-sokoban-franz/SDL2/src/atomic/../dynapi/SDL_dynapi.h \
- /home/axel/techdev-sokoban-franz/SDL2/src/atomic/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h include/SDL_atomic.h
--- SDL2/build/SDL_hints.d
+++
@@ -1,9 +0,0 @@
-build/SDL_hints.lo: /home/axel/techdev-sokoban-franz/SDL2/src/SDL_hints.c \
- /home/axel/techdev-sokoban-franz/SDL2/src/./SDL_internal.h \
- /home/axel/techdev-sokoban-franz/SDL2/src/./dynapi/SDL_dynapi.h \
- /home/axel/techdev-sokoban-franz/SDL2/src/./dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h include/SDL_hints.h \
- include/SDL_error.h \
- /home/axel/techdev-sokoban-franz/SDL2/src/SDL_hints_c.h
